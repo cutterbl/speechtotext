9d2d528cefccfd8b9ff538efc4337842b2e6a2ab
dan-smith-ul <80526834+dan-smith-ul@users.noreply.github.com>
2021-10-07 14:58:06 +0100

    base create-react-app for hackathon

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Team Speechless Hackathon</h1>
+        <a
+          className="App-link"
+          href="https://github.com/cutterbl/speechtotext"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Git
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
